3fead78c2a1f5ae73fd05fac9a6cb329def4dd26
Rishantchauhan <122939804+Rishantchauhan@users.noreply.github.com>
2024-04-05 14:57:16 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,12 +6,13 @@ const express = require('express');
 const app = express();
 const port = 5000; // Corrected port number
 
-app.use(cors({
-  origin: ["https://mern-notes-app-za97.vercel.app/"],
-  methods:["POST","GET","PUT","DELETE"],
-  credentials:true
-  // Replace with your allowed origin(s)
-}));
+app.use(cors());
+// app.use(cors({
+//   origin: ["https://mern-notes-app-za97.vercel.app/"],
+//   methods:["POST","GET","PUT","DELETE"],
+//   credentials:true
+//   // Replace with your allowed origin(s)
+// }));
 app.use(express.json());
 app.use('/api/auth', require('./Routes/auth.js'));
 app.use('/api/notes', require('./Routes/notes.js'));
